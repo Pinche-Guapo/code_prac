9e3d70a500e58f507e7d25bd5381765243c179eb
Pinche-Guapo <98554153+Pinche-Guapo@users.noreply.github.com>
2026-03-28 20:20:02 +0900

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/1001. A－B/A－B.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+    int num1 = 0;
+    int num2 = 0;
+    int ans = 0;
+    
+    std::cin >> num1;
+    std::cin >> num2;
+    
+    ans = num1 - num2;
+    std::cout << ans << std::endl;
+    
+    return 0;
+}
