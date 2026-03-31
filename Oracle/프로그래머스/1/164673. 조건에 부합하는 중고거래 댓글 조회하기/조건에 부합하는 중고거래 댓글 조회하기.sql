b30544ad7e7f948ded3bec3b3d203269f2dab096
Pinche-Guapo <98554153+Pinche-Guapo@users.noreply.github.com>
2026-04-01 01:42:50 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ Oracle/프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,2 @@
+-- 코드를 입력하세요
+SELECT b.title, b.board_id, r.reply_id, r.writer_id, r.contents, to_char(r.created_date, 'YYYY-MM-DD') as created_date FROM USED_GOODS_BOARD b, USED_GOODS_REPLY r WHERE r.board_id = b.board_id AND TO_CHAR(b.CREATED_DATE, 'YYYY-MM') = '2022-10' ORDER BY r.created_date asc, b.title asc;
